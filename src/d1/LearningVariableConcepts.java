5cbabfb6b082cd18485997a48632d1c1e0a56baf
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-12-17 22:34:22 +0530

    Variables/DataTypes/Print

---
+++ src/d1/LearningVariableConcepts.java
@@ -0,0 +1,46 @@
+package d1;
+
+public class LearningVariableConcepts {
+    public static void main(String[] args) {
+
+        // I want to add two numbers 10 & 12
+
+        int x = 10;
+        int y = 12;
+
+        //number{integer Number[byte, short, int, long], decimal Number[float, double]}, singleCharacter[char], [true/false]boolean
+
+        // 1009, 125.6, 123, 10000, 345.678
+
+        int n1 = 1009;
+        double n2 = 125.6;
+        int n3 = 123;
+        int x4 = 10000;
+        double n4 = 345.678;
+
+        //a,e,i,o,u
+
+        char x1 = 'a';
+        char x2 = 'e';
+        char x3 = 'i';
+        char x5 = 'o';
+        char x6 = 'u';
+
+        //true/false
+
+        boolean b1 = false;
+        boolean b2 = true;
+
+
+        //Charles
+
+        String t1 = "Charles";
+        //Kerrie
+
+        String t2 = "Kerrie";
+
+        // I am learning Java
+
+        String t3 = "I am learning Java";
+    }
+}
